647054d07733d33cb9da3b7f9e5a8ce99cb08c69
rajmurphy <rajirasheed39@icloud.com>
2023-10-16 11:23:13 +0000

    updated

--- _printf.c
+++ _printf.c
@@ -16,49 +16,37 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(args, format);
 
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] != '%')
-		{
-			_pchar(format[i]);
-			count++;
-		}
+			count += _pchar(format[i]);
 		else if (format[i] == '%' && format[i + 1] != '%')
 		{
 			i++;
 			spec = format[i];
 
 			if (spec == 'c')
-			{
-				_pchar(va_arg(args, int));
-				count++;
-			}
+				count += _pchar(va_arg(args, int));
 			else if (spec == 's')
 			{
 				char *str = va_arg(args, char *);
-				if (str == NULL)
-				str = "(null)";
 
+				if (str == NULL)
+					str = "(null)";
 				for (j = 0; str[j] != '\0'; j++)
-				{
-					_pchar(str[j]);
-					count++;
-				}
+					count += _pchar(str[j]);
 			}
 			else if (spec == 'd' || spec == 'i')
-			{
 				count += print_number(va_arg(args, int));
-			}
 		}
-		else if (format[i] == '%' && format[i + 1] == '%') {
-			_pchar('%');
-			count++;
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			count += _pchar('%');
 			i++;
 		}
 	}
 	va_end(args);
-	return count;
+	return (count);
 }

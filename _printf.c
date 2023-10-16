86e52dd07edf178cb71018772d0edc47d3634e4d
rajmurphy <rajirasheed39@icloud.com>
2023-10-16 11:07:34 +0000

    updated

--- _printf.c
+++ _printf.c
@@ -4,49 +4,61 @@
 * _printf - Custom printf implementation
 * @format: Format string
 * @...: Variable number of arguments
-* @n: Integer to print
-*Return: Number of characters printed
-* print_number - Print an integer
-
+* Return: Number of characters printed
 */
 
 int _printf(const char *format, ...)
 {
+
 	va_list args;
-	char spec = format[i];
-	int i, count = 0;
+	int i, j, count = 0;
+	char spec;
 
-    if (format == NULL)
-        return -1;
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
-    for (i = 0; format[i]; i++) {
-        if (format[i] != '%') {
-            _pchar(format[i]);
-            count++;
-        } else if (format[i] == '%' && format[i + 1] != '%') {
-            i++;
-            if (spec == 'c')
-                _pchar(va_arg(args, int));
-            else if (spec == 's') {
-                char *str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    _pchar(*str++);
-                    count++;
-                }
-            } else if (spec == 'd' || spec == 'i') {
-                count += print_number(va_arg(args, int));
-            }
-        } else if (format[i] == '%' && format[i + 1] == '%') {
-            _pchar('%');
-            count++;
-            i++;
-        }
-    }
+	va_start(args, format);
 
-    va_end(args);
-    return count;
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+		{
+			_pchar(format[i]);
+			count++;
+		}
+		else if (format[i] == '%' && format[i + 1] != '%')
+		{
+			i++;
+			spec = format[i];
+
+			if (spec == 'c')
+			{
+				_pchar(va_arg(args, int));
+				count++;
+			}
+			else if (spec == 's')
+			{
+				char *str = va_arg(args, char *);
+				if (str == NULL)
+				str = "(null)";
+
+				for (j = 0; str[j] != '\0'; j++)
+				{
+					_pchar(str[j]);
+					count++;
+				}
+			}
+			else if (spec == 'd' || spec == 'i')
+			{
+				count += print_number(va_arg(args, int));
+			}
+		}
+		else if (format[i] == '%' && format[i + 1] == '%') {
+			_pchar('%');
+			count++;
+			i++;
+		}
+	}
+	va_end(args);
+	return count;
 }
